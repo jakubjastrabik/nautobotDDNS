5a42322c891395a9312c2e9eaaaccb7b8051f262
Jakub Jastrabík <jakub.jastrabik@lastmile.sk>
2021-10-18 15:02:54 +0200

    Fix issue No.9 - Fix issue with importing views module

--- nautobot_ddns/views.py
+++ nautobot_ddns/views.py
@@ -12,8 +12,7 @@ from nautobot_ddns.background_tasks import dns_create
 from nautobot_ddns.forms import ExtraDNSNameEditForm
 from nautobot_ddns.models import DNSStatus, ExtraDNSName
 from nautobot_ddns.utils import normalize_fqdn
-from nautobot.utilities.views import ObjectDeleteView, ObjectEditView
-
+from nautobot.core.views.generic import ObjectDeleteView, ObjectEditView
 
 # noinspection PyMethodMayBeStatic
 class ExtraDNSNameObjectMixin:

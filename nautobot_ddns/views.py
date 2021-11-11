8f0ec2453d45892ccab592850cfdb7571383d633
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-11 14:53:18 +0100

    Hmm

--- nautobot_ddns/views.py
+++ nautobot_ddns/views.py
@@ -13,16 +13,7 @@ from nautobot_ddns.forms import ExtraDNSNameEditForm
 from nautobot_ddns.models import DNSStatus, ExtraDNSName
 from nautobot_ddns.utils import normalize_fqdn
 
-#from nautobot.core.views.generic import ObjectDeleteView, ObjectEditView
-from nautobot.utilities.views import ObjectDeleteView, ObjectEditView
-
-# try:
-
-#     from nautobot.utilities.views import ObjectDeleteView, ObjectEditView
-# except ImportError:
-
-#     from 
-
+from nautobot.core.views.generic import ObjectDeleteView, ObjectEditView
 
 # noinspection PyMethodMayBeStatic
 class ExtraDNSNameObjectMixin:

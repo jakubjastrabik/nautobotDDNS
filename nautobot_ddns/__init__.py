f1ffacc52ed2167407a10b61e3e5bea673b3d022
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-11 10:25:38 +0100

    Add app init config

---
+++ nautobot_ddns/__init__.py
@@ -0,0 +1,27 @@
+VERSION = '0.0.1'
+
+try:
+    from nautobot.extras.plugins import PluginConfig
+except ImportError:
+    # Dummy for when importing outside of Nautobot
+    class PluginConfig:
+        pass
+
+class NautobotDDNSConfig(PluginConfig):
+    name = 'nautobot_ddns'
+    verbose_name = 'Dynamic DNS'
+    version = VERSION
+    author = 'Jakub Jastrabik'
+    author_email = 'jastrabik.kubko@gmail.com'
+    description = 'Dynamic DNS Connector for Nautobot'
+    base_url = 'ddns'
+    required_settings = []
+    default_settings = {}
+
+    def ready(self):
+        super().ready()
+
+        from . import signals
+
+
+config = NautobotDDNSConfig

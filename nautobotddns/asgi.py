399bca55d6dfcd44a406bc9de4d6a4418fc271d9
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-11 10:05:59 +0100

    Temp rename app

--- nautobotddns/asgi.py
+++ nautobotddns/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nautobot_ddns project.
+ASGI config for nautobotddns project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nautobot_ddns.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nautobotddns.settings')
 
 application = get_asgi_application()

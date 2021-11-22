eb3aa56767f5d521bc0d1e44d8acc5c06974c28a
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-22 14:26:09 +0100

    Change some stuff

--- nautobot_ddns/forms.py
+++ nautobot_ddns/forms.py
@@ -1,11 +1,7 @@
-import logging
-
 from django import forms
 
 from nautobot_ddns.models import ExtraDNSName
 from nautobot.utilities.forms import BootstrapMixin
-
-logger = logging.getLogger('nautobot_ddns')
 
 class ExtraDNSNameEditForm(BootstrapMixin, forms.ModelForm):
     class Meta:

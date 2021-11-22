89fc7454485618bcc16e66d97b845f7d58320f9f
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-22 13:57:41 +0100

    Change some stuff

--- nautobot_ddns/urls.py
+++ nautobot_ddns/urls.py
@@ -4,8 +4,8 @@ from . import views
 # from .views import ExtraDNSNameCreateView, ExtraDNSNameDeleteView, ExtraDNSNameEditView, IPAddressDNSNameRecreateView
 
 urlpatterns = [
-    path("ip-addresses/<uuid:pk>/recreate/", views.IPAddressDNSNameRecreateView.as_view(), name='ipaddress_dnsname_recreate'),
-    path("ip-addresses/<uuid:pk>/extra/create/", views.ExtraDNSNameCreateView.as_view(), name='extradnsname_create'),
-    path("ip-addresses/<uuid:pk>/extra/<int:pk>/edit/", views.ExtraDNSNameEditView.as_view(), name='extradnsname_edit'),
-    path("ip-addresses/<uuid:pk>/extra/<int:pk>/delete/", views.ExtraDNSNameDeleteView.as_view(), name='extradnsname_delete'),
+    path("ip-addresses/<uuid:ipaddress_pk>/recreate/", views.IPAddressDNSNameRecreateView.as_view(), name='ipaddress_dnsname_recreate'),
+    path("ip-addresses/<uuid:ipaddress_pk>/extra/create/", views.ExtraDNSNameCreateView.as_view(), name='extradnsname_create'),
+    path("ip-addresses/<uuid:ipaddress_pk>/extra/<int:pk>/edit/", views.ExtraDNSNameEditView.as_view(), name='extradnsname_edit'),
+    path("ip-addresses/<uuid:ipaddress_pk>/extra/<int:pk>/delete/", views.ExtraDNSNameDeleteView.as_view(), name='extradnsname_delete'),
 ]

4d3379b17a62462cb15b949507c4807a90d92e01
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-22 10:48:44 +0100

    Change privilages

--- nautobot_ddns/migrations/0001_initial.py
+++ nautobot_ddns/migrations/0001_initial.py
@@ -57,4 +57,38 @@ class Migration(migrations.Migration):
                 'ordering': ('prefix',),
             },
         ),
+        migrations.CreateModel(
+            name='ExtraDNSName',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=255, validators=[nautobot_ddns.validators.HostnameValidator()])),
+                ('last_update', models.DateTimeField(auto_now=True)),
+                ('forward_action', models.PositiveSmallIntegerField(blank=True, null=True)),
+                ('forward_rcode', models.PositiveIntegerField(blank=True, null=True)),
+                ('reverse_action', models.PositiveSmallIntegerField(blank=True, null=True)),
+                ('reverse_rcode', models.PositiveIntegerField(blank=True, null=True)),
+                ('ip_address', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='ipam.ipaddress')),
+            ],
+            options={
+                'verbose_name': 'extra DNS name',
+                'verbose_name_plural': 'extra DNS names',
+                'unique_together': {('ip_address', 'name')},
+            },
+        ),
+        migrations.CreateModel(
+            name='DNSStatus',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False)),
+                ('last_update', models.DateTimeField(auto_now=True)),
+                ('forward_action', models.PositiveSmallIntegerField(blank=True, null=True)),
+                ('forward_rcode', models.PositiveIntegerField(blank=True, null=True)),
+                ('reverse_action', models.PositiveSmallIntegerField(blank=True, null=True)),
+                ('reverse_rcode', models.PositiveIntegerField(blank=True, null=True)),
+                ('ip_address', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='ipam.ipaddress')),
+            ],
+            options={
+                'verbose_name': 'DNS status',
+                'verbose_name_plural': 'DNS status',
+            },
+        ),
     ]

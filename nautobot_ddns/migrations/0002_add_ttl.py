19f1f2d06249d8d2513376e7f7e82444d82dd261
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-22 10:15:37 +0100

    Change privilages

--- nautobot_ddns/migrations/0002_add_ttl.py
+++ nautobot_ddns/migrations/0002_add_ttl.py
@@ -3,9 +3,10 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    # dependencies = [
-    #     ('nautobot_ddns', '0001_initial'),
-    # ]
+
+    dependencies = [
+        ('nautobot_ddns', '0001_initial'),
+    ]
 
     # operations = [
     #     migrations.AddField(

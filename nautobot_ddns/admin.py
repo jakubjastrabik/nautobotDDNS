583013a90281dab7b2b8ffaed2361111f69853aa
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-11-23 16:24:45 +0100

    hmm no .322

--- nautobot_ddns/admin.py
+++ nautobot_ddns/admin.py
@@ -125,7 +125,7 @@ class ReverseZoneAdmin(admin.ModelAdmin):
             logger.error(zone.prefix)
 
             # Find all more-specific zones
-            more_specifics = ReverseZone.objects.net_contained(zone.prefix)
+            more_specifics = ReverseZone.objects.filter(prefix=zone.prefix)
             
             logger.error(more_specifics)   
 

81208238ae87368fb5b2c222ce2237dfbf98c980
Jakub Jastrabik <jakub.jastrabik@lastmile.sk>
2021-12-13 18:48:21 +0100

    Try some staff

--- nautobot_ddns/admin.py
+++ nautobot_ddns/admin.py
@@ -125,7 +125,8 @@ class ReverseZoneAdmin(admin.ModelAdmin):
 
             # Find all more-specific zones
             # more_specifics = ReverseZone.objects.filter(prefix=zone.prefix).exclude(pk=zone.pk)
-            logger.fatal(ReverseZone.objects)
+            
+            # logger.fatal(ReverseZone.objects)
 
             # logger.error(more_specifics)   
 
